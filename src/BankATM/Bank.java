535c3938007a586292da9b3f7a20831df0d3e1f5
Melikinho <Jimmymelik10@gmail.com>
2020-12-03 16:57:33 +0100

    Initizialing the showMenu for the Bank ATM application. Switch case also is initizialed.

--- src/BankATM/Bank.java
+++ src/BankATM/Bank.java
@@ -36,5 +36,48 @@ public class Bank {
         } else System.out.println("No Transaction occured.");
     }
 
+    void bankingMenu(){
+        char option = '\0';
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("*******WELCOME TO MELIKINHO'S BANK*******");
+        System.out.println("******* WELCOME ******* " + customerName + "" + customerMiddlename + "" + customerLastname);
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.println("A.) Check Balance");
+        System.out.println("B.) Deposit cash onto ATM");
+        System.out.println("C.) Withdraw cash from ATM");
+        System.out.println("D.) See your previous transaction: ");
+        System.out.println("E.) Exit your BANK ATM");
+
+        do {
+            System.out.println("************************************");
+            System.out.println("Enter your option A to E");
+            option = scanner.next ().charAt (0);
+            System.out.println("\n");
+
+            switch (option){
+                case 'A':
+                    System.out.println("*********");
+                    System.out.println("Balance: " + balance);
+                    System.out.println("*********");
+                    System.out.println("\n");
+                case 'B':
+                    System.out.println("*********");
+                    System.out.println("Enter an amount to deposit cash: ");
+                    System.out.println("*********");
+                    int amount = scanner.nextInt ();
+                    deposit(amount);
+                    System.out.println("\n");
+
+
+            }
+        }while(option != 'E');
+        System.out.println("Thank you for using our services. Welcome back!");
+        System.out.println("********* BANK MELIKINHO *********");
+
+
+    }
+
     }
 
